aad50fdbbb71bb1f8d7f9f787b4965c2899c32b9
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-11-28 12:05:22 +0530

    Create Calculator.py
    You have to understand how numbers are working in python. you can do any type of mathematical operation in python. Just open your interpreter, type any sort of calculation you want to do

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+#Add two numbers
+5+3
+Output:8
+#Subtract two Numbers
+10 - 5
+Output:5
+#Multiplying two numbers
+5 * 5
+Output:25
+#Division of two numbers
+# It outputs the quotient and result is float
+10/5
+output: 2.0
+#It outputs the remainder 
+10%5
+output:0
+#It outputs the quotient and the result is rounded to the smallest whole integer
+10//4
+output:2
