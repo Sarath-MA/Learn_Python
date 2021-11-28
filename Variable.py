613c41ae1567e6a26880d0d16da76b8f2ce6ad83
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-11-28 12:12:24 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,19 @@
+#Variable: The name refers to the value or memory holder. Here value means basic unit of data.
+#Assign variable names
+a = 5
+a1 = 55
+b = "Hello world"
+_b = "New to data science"
+
+
+#To name a variable it has some own rules and restrictions:
+#1. It cannot start with a number
+#2. It contains any alphanumeric characters(a1 = 5)
+#3. Cannot contain keywords(True, False)
+#4. Cannot start with number(1a = 3)
+#5. Only starts with a letter or underscore.
+#Your variable name has to be descriptive and readable. For example, if you have stored an age of a person and his name use to describe the information what the variable has.
+
+#For Example:
+  Age = 50
+  Name = 'John'
